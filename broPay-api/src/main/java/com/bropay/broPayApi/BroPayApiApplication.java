a64770a2aed7cea9e49d2e8495456665d14bbe9b
vlh-05 <haindu.15@gmail.com>
2025-06-30 16:20:07 -0500

    Sprint 1 complete: Dockerized backend and frontend with health check

---
+++ broPay-api/src/main/java/com/bropay/broPayApi/BroPayApiApplication.java
@@ -0,0 +1,13 @@
+package com.bropay.broPayApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroPayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroPayApiApplication.class, args);
+	}
+
+}
---
+++ broPay-api/src/test/java/com/bropay/broPayApi/BroPayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bropay.broPayApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroPayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
